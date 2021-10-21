d4ce69c6609a3089698fbf373ac0a052eeb1c4a4
EricShimomoto <eric.shimomoto@gmail.com>
2021-10-21 14:19:56 +0900

    modifications anda database

---
+++ Project01.py
@@ -0,0 +1,107 @@
+from os import stat_result
+from tkinter import *
+from PIL import ImageTk, Image
+import sqlite3
+
+root = Tk()
+root.title('My first datapbase applicartion')
+root.geometry("400x400")
+
+#Database
+
+#Create a database or connect
+conn = sqlite3.connect('address_book.db')
+
+#Create cursor
+c = conn.cursor()
+
+# Create a table
+# c.execute("""CREATE TABLE addresses(
+#         first_name text, 
+#         last_name text,
+#         address text,
+#         city text,
+#         state text,
+#         zipcode integer
+#         )""")
+
+#Create labels
+f_name_label = Label(root,text='First Name')
+f_name_label.grid(row=0,column=0,padx=20)
+l_name_label = Label(root,text='Last Name')
+l_name_label.grid(row=1,column=0)
+add_label = Label(root,text='Address')
+add_label.grid(row=2,column=0)
+city_label = Label(root,text='City')
+city_label.grid(row=3,column=0,padx=20)
+state_label = Label(root,text='State')
+state_label.grid(row=4,column=0,padx=20)
+z_code_label = Label(root,text='Zip Code')
+z_code_label.grid(row=5,column=0,padx=20)
+
+
+#Create text box
+f_name = Entry(root,width=30)
+f_name.grid(row=0,column=1,padx=20)
+l_name = Entry(root,width=30)
+l_name.grid(row=1,column=1)
+add = Entry(root,width=30)
+add.grid(row=2,column=1)
+city = Entry(root,width=30)
+city.grid(row=3,column=1)
+state = Entry(root,width=30)
+state.grid(row=4,column=1)
+z_code = Entry(root,width=30)
+z_code.grid(row=5,column=1)
+
+#create submit button
+subimit_but = Button(root, text="Add", command = lambda:submit())
+subimit_but.grid(row=6,column=0, columnspan=2, pady=10,padx=10, ipadx=150)
+
+#create submit function to database
+def submit():
+
+    #Create a database or connect
+    conn = sqlite3.connect('address_book.db')
+
+    #Create cursor
+    c = conn.cursor()
+
+    #Inser into a table
+    c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :add, :city, :state, :zipcode)",
+            {
+                'f_name': f_name.get(),
+                'l_name': l_name.get(),
+                'add': add.get(),
+                'city': city.get(),
+                'state' : state.get(),
+                'zipcode' : z_code.get()
+            }    
+        )
+
+    #commit changes
+    conn.commit()
+
+    #Close connection
+    conn.close()
+
+
+
+    #clear the text boxes
+    f_name.delete(0)
+    l_name.delete(0)
+    add.delete(0)
+    city.delete(0)
+    state.delete(0)
+    z_code.delete(0)
+
+    pass
+
+#commit changes
+conn.commit()
+
+#Close connection
+conn.close()
+
+
+root.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+c.execute("""CREATE TABLE addresses(
+#         first_name text, 
+#         last_name text,
+#         address text,
+#         city text,
+#         state text,
+#         zipcode integer
+#         )""")
